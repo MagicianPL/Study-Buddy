ea5f4112075d5314d43a3986882258b81dfffb01
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-13 20:46:07 +0200

    fun with styledcomponents

--- src/Components/UserItemList/UserItemList.js
+++ src/Components/UserItemList/UserItemList.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-const UserItemList = ( {userData: {average, name, attendance}}) => (
-    <li>
-          <div>{average}</div>
-          <div>
-            <p>{name}</p>
-            <p>{attendance}</p>
-          </div>
-          <button>x</button>
-        </li>
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  &:not(:last-child)::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+const UserItemList = ({ userData: { average, name, attendance } }) => (
+  <Wrapper>
+    <div>{average}</div>
+    <div>
+      <p>{name}</p>
+      <p>{attendance}</p>
+    </div>
+    <button>x</button>
+  </Wrapper>
 );
 
 export default UserItemList;
--- src/Components/UsersList/UsersList.js
+++ src/Components/UsersList/UsersList.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { users } from "../../Data/Users";
 import UserItemList from "../UserItemList/UserItemList";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
+`;
 
 const UsersList = () => (
-  <div>
+  <Wrapper>
     <ul>
-      {users.map( userData => (
+      {users.map((userData) => (
         <UserItemList userData={userData} />
       ))}
     </ul>
-  </div>
+  </Wrapper>
 );
 
 export default UsersList;
