9d98c254372e40ffcfc629b98d399a3b111fb2bb
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-13 23:18:54 +0200

    theme provider

--- src/Components/Organisms/UsersList/UsersList.js
+++ src/Components/Organisms/UsersList/UsersList.js
@@ -4,7 +4,7 @@ import UserItemList from "../../Molecules/UserItemList/UserItemList";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: white;
+  background-color: ${({ theme }) => theme.colors.white};
   width: 100%;
   max-width: 500px;
   padding: 40px 30px;
