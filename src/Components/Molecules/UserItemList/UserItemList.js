6e9a00d375179f8611ea615a4ea4ebd66020ea1a
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-14 23:40:28 +0200

    possibility to delete student

--- src/Components/Molecules/UserItemList/UserItemList.js
+++ src/Components/Molecules/UserItemList/UserItemList.js
@@ -1,16 +1,19 @@
 import React from "react";
-import {Wrapper} from "./UserItemList.styles"
+import { Wrapper } from "./UserItemList.styles";
 import { StyledAverage } from "../../Atoms/StyledAverage/StyledAverage";
 import UserInfo from "../../Atoms/UserInfo/UserInfo";
 import Button from "../../Atoms/Button/Button";
 
-
-
-const UserItemList = ({ userData: { average, name, attendance } }) => (
+const UserItemList = ({ deleteUser, userData: { average, name, attendance } }
+) => (
   <Wrapper>
     <StyledAverage average={average}>{average}</StyledAverage>
     <UserInfo name={name} attendance={attendance} />
-    <Button />
+    <Button
+      onClick={() => {
+        deleteUser(name);
+      }}
+    />
   </Wrapper>
 );
 
--- src/Components/Organisms/UsersList/UsersList.js
+++ src/Components/Organisms/UsersList/UsersList.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { users } from "../../../Data/Users";
 import UserItemList from "../../Molecules/UserItemList/UserItemList";
 import { Wrapper, StyledList } from "./UsersList.styles";
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData) => (
-        <UserItemList userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+const UsersList = () => {
+  const [usersList, setUsersList] = useState(users);
+
+  const deleteUser = (userName) => {
+    const filteredArr = usersList.filter((user) => user.name !== userName);
+
+    setUsersList(filteredArr);
+  };
+
+  return (
+    <Wrapper>
+      <h1>Students List</h1>
+      <StyledList>
+        {usersList.map((userData, index) => (
+          <UserItemList
+            userData={userData}
+            deleteUser={deleteUser}
+            key={userData.name}
+          />
+        ))}
+      </StyledList>
+    </Wrapper>
+  );
+};
 
 export default UsersList;
