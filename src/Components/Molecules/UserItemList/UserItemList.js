93dfc1a50a5bcee05e64ab3d0babdc2026eb3574
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-14 21:30:26 +0200

    styled components on another files

--- src/Components/Molecules/UserItemList/UserItemList.js
+++ src/Components/Molecules/UserItemList/UserItemList.js
@@ -1,23 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import {Wrapper} from "./UserItemList.styles"
 import { StyledAverage } from "../../Atoms/StyledAverage/StyledAverage";
 import UserInfo from "../../Atoms/UserInfo/UserInfo";
 import Button from "../../Atoms/Button/Button";
 
-const Wrapper = styled.li`
-  display: flex;
-  align-items: center;
-  position: relative;
 
-  &:not(:last-child)::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 1px;
-    background-color: lightgrey;
-  }
-`;
 
 const UserItemList = ({ userData: { average, name, attendance } }) => (
   <Wrapper>
