b42768a1b4893467fff645a31c62dc6aa3882fe5
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-15 18:15:37 +0200

    form in progress

--- src/Components/Molecules/FormField/FormField.js
+++ src/Components/Molecules/FormField/FormField.js
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import { Label } from "../../Atoms/Label/Label.styled";
 import { Input } from "../../Atoms/Input/Input.styled";
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+
+  label {
+    margin: 10px 0;
+  }
+`;
+
 const FormField = ({ label, name, id, type = "text" }) => {
   return (
-    <>
+    <Wrapper>
       <Label htmlFor={id}>{label}</Label>
       <Input name={name} id={id} type={type} />
-    </>
+    </Wrapper>
   );
 };
+
+export default FormField;
--- src/Components/Organisms/UsersList/UsersList.js
+++ src/Components/Organisms/UsersList/UsersList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { users } from "../../../Data/Users";
 import UserItemList from "../../Molecules/UserItemList/UserItemList";
 import { Wrapper, StyledList } from "./UsersList.styles";
+import FormField from "../../Molecules/FormField/FormField";
 
 const UsersList = () => {
   const [usersList, setUsersList] = useState(users);
@@ -13,18 +14,26 @@ const UsersList = () => {
   };
 
   return (
-    <Wrapper>
-      <h1>Students List</h1>
-      <StyledList>
-        {usersList.map((userData, index) => (
-          <UserItemList
-            userData={userData}
-            deleteUser={deleteUser}
-            key={userData.name}
-          />
-        ))}
-      </StyledList>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <h1>Add Student</h1>
+        <FormField label="Name" name="name" id="name" />
+        <FormField label="Attendance" name="attendance" id="attendance" />
+        <FormField label="Average" name="average" id="average" />
+      </Wrapper>
+      <Wrapper>
+        <h1>Students List</h1>
+        <StyledList>
+          {usersList.map((userData, index) => (
+            <UserItemList
+              userData={userData}
+              deleteUser={deleteUser}
+              key={userData.name}
+            />
+          ))}
+        </StyledList>
+      </Wrapper>
+    </>
   );
 };
 
