c08a2cbdad97c911aa827d6464393fc6a96273f3
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-20 23:39:24 +0200

    fun with tests

---
+++ src/Components/Molecules/FormField/FormField.test.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+const InputWithButton = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        type="text"
+        name="Name"
+        id="name"
+        placeholder="Enter your name"
+      />
+      <button disabled={!inputValue}>SUBMIT</button>
+    </>
+  );
+};
+
+describe("InputWithButton", () => {
+  it("renders the component", () => {
+    render(<InputWithButton />);
+    screen.getByText("SUBMIT");
+  });
+
+  it("properly handles value change", () => {
+    render(<InputWithButton />);
+    const input = screen.getByPlaceholderText("Enter your name");
+    fireEvent.change(input, { target: { value: "Roman" } });
+    expect(input).toHaveValue("Roman");
+  });
+});
--- src/index.test.js
+++
@@ -1,5 +0,0 @@
-const add = (a, b) => a + b;
-
-it("adds two values", () => {
-  expect(add(10, 10)).not.toBe(30);
-});
